33e2d3817ce246f3ecd9c0f8844edeb08e282afa
ysavchen <yura_savchenko@outlook.com>
2019-12-20 17:58:39 +0300

    update ConsolePrintService

--- hw-l01-spring-introduction/src/main/java/com/mycompany/hw_l01_spring_introduction/service/ConsolePrintService.java
+++ hw-l01-spring-introduction/src/main/java/com/mycompany/hw_l01_spring_introduction/service/ConsolePrintService.java
@@ -1,10 +1,14 @@
 package com.mycompany.hw_l01_spring_introduction.service;
 
+import java.io.PrintStream;
+
 public class ConsolePrintService implements PrintService {
+
+    private final PrintStream out = System.out;
 
     @Override
     public void print(String text) {
-        System.out.println(text);
+        out.println(text);
     }
 
 }
