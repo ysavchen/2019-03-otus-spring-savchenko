9d35cb29eb02863b84b1544087f2f2472b65d758
ysavchen <yura_savchenko@outlook.com>
2020-01-05 00:30:41 +0300

    add ConsoleIOServiceTests

---
+++ hw-l02-spring-config/src/test/java/com/mycompany/hw_l02_spring_config/ConsoleIOServiceTests.java
@@ -0,0 +1,54 @@
+package com.mycompany.hw_l02_spring_config;
+
+import com.mycompany.hw_l02_spring_config.service.ConsoleContext;
+import com.mycompany.hw_l02_spring_config.service.ConsoleIOService;
+import com.mycompany.hw_l02_spring_config.service.IOService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ConsoleIOServiceTests {
+
+    @Mock
+    private ConsoleContext consoleContext;
+
+    @Mock
+    private PrintStream printStream;
+
+    private Scanner scanner;
+
+    private IOService ioService;
+
+    @BeforeEach
+    void setUp() {
+        scanner = new Scanner("test");
+        ioService = new ConsoleIOService(consoleContext);
+    }
+
+    @Test
+    void testOut() {
+        var captor = ArgumentCaptor.forClass(String.class);
+        when(consoleContext.out()).thenReturn(printStream);
+        doNothing().when(printStream).println(captor.capture());
+        ioService.out("test");
+
+        assertEquals("test", captor.getValue());
+    }
+
+    @Test
+    void testReadString() {
+        when(consoleContext.scanner()).thenReturn(scanner);
+        assertEquals("test", ioService.readString());
+    }
+}
