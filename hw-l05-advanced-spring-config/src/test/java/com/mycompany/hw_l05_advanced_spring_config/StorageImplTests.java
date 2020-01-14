a52e39dc492ca0f3af945df114c95673f2c1582e
ysavchen <yura_savchenko@outlook.com>
2020-01-14 23:22:45 +0300

    add StorageImplTests

---
+++ hw-l05-advanced-spring-config/src/test/java/com/mycompany/hw_l05_advanced_spring_config/StorageImplTests.java
@@ -0,0 +1,38 @@
+package com.mycompany.hw_l05_advanced_spring_config;
+
+import com.mycompany.hw_l05_advanced_spring_config.app.AppLocale;
+import com.mycompany.hw_l05_advanced_spring_config.dao.Storage;
+import com.mycompany.hw_l05_advanced_spring_config.domain.Answer;
+import com.mycompany.hw_l05_advanced_spring_config.domain.Question;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class StorageImplTests {
+
+    @MockBean
+    private AppLocale appLocale;
+
+    @Autowired
+    private Storage storage;
+
+    @Test
+    void getQuestionsTest() {
+        when(appLocale.getLocale()).thenReturn(Locale.US);
+        List<Question> questions = storage.getQuestions();
+
+        var question = new Question(
+                1, "question one?",
+                List.of("1", "15", "6", "37"), new Answer(1, "15")
+        );
+        assertThat(questions).contains(question);
+    }
+}
