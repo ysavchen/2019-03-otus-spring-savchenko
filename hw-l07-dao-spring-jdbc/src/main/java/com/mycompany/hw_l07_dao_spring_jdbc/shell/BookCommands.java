a8ed2f73d164d20d753ddd9db7fe68af0172ba26
ysavchen <yura_savchenko@outlook.com>
2020-01-27 23:59:38 +0300

    rough BookCommands

--- hw-l07-dao-spring-jdbc/src/main/java/com/mycompany/hw_l07_dao_spring_jdbc/shell/BookCommands.java
+++ hw-l07-dao-spring-jdbc/src/main/java/com/mycompany/hw_l07_dao_spring_jdbc/shell/BookCommands.java
@@ -1,9 +1,9 @@
 package com.mycompany.hw_l07_dao_spring_jdbc.shell;
 
-import com.mycompany.hw_l07_dao_spring_jdbc.dao.BookDao;
 import com.mycompany.hw_l07_dao_spring_jdbc.domain.Author;
 import com.mycompany.hw_l07_dao_spring_jdbc.domain.Book;
 import com.mycompany.hw_l07_dao_spring_jdbc.domain.Genre;
+import com.mycompany.hw_l07_dao_spring_jdbc.service.BookDbService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -12,7 +12,7 @@ import org.springframework.shell.standard.ShellMethod;
 @RequiredArgsConstructor
 public class BookCommands {
 
-    private final BookDao bookDao;
+    private final BookDbService bookDbService;
 
     @ShellMethod(value = "Add book", key = {"ab", "add-book"})
     public String addBook(String title, String authorName,
@@ -21,12 +21,7 @@ public class BookCommands {
         var author = new Author(authorName, authorSurname);
         var book = new Book(title, author, new Genre(genre));
 
-        return "Added book with id = " + bookDao.insert(book);
-    }
-
-    @ShellMethod(value = "Find book by id", key = {"fbi", "find-book-by-id"})
-    public String findBookById(String title) {
-        return "";
+        return "Added book with id = " + bookDbService.insert(book);
     }
 
     @ShellMethod(value = "Find book by title", key = {"fbt", "find-book-by-title"})
@@ -34,9 +29,30 @@ public class BookCommands {
         return "";
     }
 
+    @ShellMethod(value = "Update title for a book", key = {"utb", "update-title-for-book"})
+    public String updateTitle(long id, String newTitle) {
+
+
+        return "Title is changed for a book with id = " + id;
+    }
+
+    @ShellMethod(value = "Update author for a book", key = {"uab", "update-author-for-book"})
+    public String updateAuthor(long id, String newAuthor) {
+
+        return "Author is changed for a book with id = " + id;
+    }
+
+    @ShellMethod(value = "Update genre for a book", key = {"ugb", "update-genre-for-book"})
+    public String updateGenre(long id, String newGenre) {
+
+
+        return "Genre is changed for a book with id = " + id;
+    }
+
     @ShellMethod(value = "Delete book by id", key = {"dbi", "delete-book-by-id"})
-    public String deleteBookById(long id) {
-        bookDao.deleteById(id);
+    public String deleteBookByTitle(long id) {
+
+
         return "Deleted book with id = " + id;
     }
 
