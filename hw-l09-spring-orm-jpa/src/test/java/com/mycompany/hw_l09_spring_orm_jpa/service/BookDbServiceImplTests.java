a70cb96c0396e583a37e4e12e76dc7105d3855a2
ysavchen <yura_savchenko@outlook.com>
2020-02-21 00:53:22 +0300

    add mock for BookRepository

--- hw-l09-spring-orm-jpa/src/test/java/com/mycompany/hw_l09_spring_orm_jpa/service/BookDbServiceImplTests.java
+++ hw-l09-spring-orm-jpa/src/test/java/com/mycompany/hw_l09_spring_orm_jpa/service/BookDbServiceImplTests.java
@@ -1,19 +1,23 @@
 package com.mycompany.hw_l09_spring_orm_jpa.service;
 
 import com.mycompany.hw_l09_spring_orm_jpa.domain.Book;
-import com.mycompany.hw_l09_spring_orm_jpa.repositories.BookRepositoryImpl;
-import com.mycompany.hw_l09_spring_orm_jpa.service.BookDbServiceImpl;
+import com.mycompany.hw_l09_spring_orm_jpa.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @DataJpaTest
-@Import({BookRepositoryImpl.class, BookDbServiceImpl.class})
+@Import(BookDbServiceImpl.class)
 public class BookDbServiceImplTests {
+
+    @MockBean
+    private BookRepository bookRepository;
 
     @Autowired
     private BookDbServiceImpl bookDbService;
@@ -21,6 +25,7 @@ public class BookDbServiceImplTests {
     @Test
     void updateBookWithTitle() {
         var book = new Book(1, "newTitle");
+        when(bookRepository.update(book)).thenReturn(true);
         assertTrue(bookDbService.update(book),
                 "Book title is not updated");
     }
