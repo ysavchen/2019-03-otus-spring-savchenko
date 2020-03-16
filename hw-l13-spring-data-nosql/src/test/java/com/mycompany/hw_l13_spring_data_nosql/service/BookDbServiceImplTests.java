08ef4ad50cd9b79c10747c26c727284512ce41b1
ysavchen <yura_savchenko@outlook.com>
2020-03-16 15:33:36 +0300

    fix BookDbServiceImplTests

--- hw-l13-spring-data-nosql/src/test/java/com/mycompany/hw_l13_spring_data_nosql/service/BookDbServiceImplTests.java
+++ hw-l13-spring-data-nosql/src/test/java/com/mycompany/hw_l13_spring_data_nosql/service/BookDbServiceImplTests.java
@@ -9,6 +9,7 @@ import com.mycompany.hw_l13_spring_data_nosql.repositories.CommentRepository;
 import com.mycompany.hw_l13_spring_data_nosql.repositories.GenreRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -16,15 +17,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @Import(BookDbServiceImpl.class)
 @ExtendWith(SpringExtension.class)
 public class BookDbServiceImplTests {
 
-    private final Genre genre = new Genre( "Computers & Technology");
-    private final Author author = new Author( "Philip", "Pratt");
-    private final Book book = new Book( "A Guide to SQL");
+    private final Genre genre = new Genre("Computers & Technology").setId("1");
+    private final Author author = new Author("Philip", "Pratt").setId("1");
+    private final Book book = new Book("A Guide to SQL").setId("1");
 
     @MockBean
     private BookRepository bookRepository;
@@ -42,16 +44,38 @@ public class BookDbServiceImplTests {
     private BookDbServiceImpl bookDbService;
 
     @Test
+    void saveWithAuthor() {
+        when(bookRepository.save(book)).thenReturn(book);
+
+        book.setAuthor(author);
+        bookDbService.save(book);
+        verify(authorRepository, atMostOnce()).save(author);
+    }
+
+    @Test
+    void saveWithoutAuthor() {
+        when(bookRepository.save(book)).thenReturn(book);
+
+        book.setAuthor(null);
+        bookDbService.save(book);
+        verify(authorRepository, never()).save(author);
+    }
+
+    @Test
     void updateTitle() {
+        when(bookRepository.findById(book.getId())).thenReturn(Optional.of(book));
+        var captor = ArgumentCaptor.forClass(Book.class);
+
         var newTitle = "New title";
         bookDbService.updateTitle(book.getId(), newTitle);
-        verify(bookRepository, atMostOnce()).updateTitle(book.getId(), newTitle);
+        verify(bookRepository).save(captor.capture());
+        assertThat(captor.getValue().getTitle()).isEqualTo(newTitle);
     }
 
     @Test
     void updateBookWithNullTitle() {
         bookDbService.updateTitle(book.getId(), null);
-        verify(bookRepository, never()).updateTitle(book.getId(), null);
+        verify(bookRepository, never()).save(book);
     }
 
     @Test
