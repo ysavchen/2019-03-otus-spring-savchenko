dd00b5c733abbad4cdc7f6c978fd1964659c80f4
ysavchen <yura_savchenko@outlook.com>
2020-03-19 12:40:50 +0300

    Remove @Document in Genre

--- hw-l13-spring-data-nosql/src/main/java/com/mycompany/hw_l13_spring_data_nosql/domain/Genre.java
+++ hw-l13-spring-data-nosql/src/main/java/com/mycompany/hw_l13_spring_data_nosql/domain/Genre.java
@@ -2,15 +2,11 @@ package com.mycompany.hw_l13_spring_data_nosql.domain;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "genres")
 @Accessors(chain = true)
 public class Genre {
 
-    @Id
     private String id;
     private String name;
 
