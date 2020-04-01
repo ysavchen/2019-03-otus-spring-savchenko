a91ebf533199c74793d49100a57ac5e92dbcaca3
ysavchen <yura_savchenko@outlook.com>
2020-04-01 19:43:13 +0300

    Rough getAllBooks()

--- hw-l16-spring-mvc-view/src/main/java/com/mycompany/hw_l16_spring_mvc_view/controllers/BookController.java
+++ hw-l16-spring-mvc-view/src/main/java/com/mycompany/hw_l16_spring_mvc_view/controllers/BookController.java
@@ -1,6 +1,8 @@
 package com.mycompany.hw_l16_spring_mvc_view.controllers;
 
 import com.mycompany.hw_l16_spring_mvc_view.dto.BookDto;
+import com.mycompany.hw_l16_spring_mvc_view.service.BookDbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 @Controller
+@RequiredArgsConstructor
 public class BookController {
+
+    private static final String BOOK_LIST_FORM = "books/bookList";
+
+    private final BookDbService dbService;
 
     @PostMapping("/book")
     public String addBook(@RequestBody BookDto bookDto, Model model) {
@@ -19,5 +30,16 @@ public class BookController {
     @GetMapping("/book/{id}")
     public String getBookById(@PathVariable("id") long id, Model model) {
         return "";
+    }
+
+    @GetMapping("/book/all")
+    public String getAllBooks(Model model) {
+        List<BookDto> books = dbService.getAllBooks()
+                .stream()
+                .map(BookDto::toDto)
+                .collect(toList());
+
+        model.addAttribute("books", books);
+        return BOOK_LIST_FORM;
     }
 }
