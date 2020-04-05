edfc3e9d79302fd0662cd834dc9dea8730e6c040
ysavchen <yura_savchenko@outlook.com>
2020-04-05 23:36:36 +0300

    Remove addAuthor and authorList

--- hw-l16-spring-mvc-view/src/main/java/com/mycompany/hw_l16_spring_mvc_view/controllers/AuthorController.java
+++ hw-l16-spring-mvc-view/src/main/java/com/mycompany/hw_l16_spring_mvc_view/controllers/AuthorController.java
@@ -1,6 +1,5 @@
 package com.mycompany.hw_l16_spring_mvc_view.controllers;
 
-import com.mycompany.hw_l16_spring_mvc_view.domain.Author;
 import com.mycompany.hw_l16_spring_mvc_view.dto.AuthorDto;
 import com.mycompany.hw_l16_spring_mvc_view.service.AuthorDbService;
 import lombok.RequiredArgsConstructor;
@@ -8,24 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
 public class AuthorController {
 
-    private static final String ADD_AUTHOR_FORM = "authors/addAuthor";
     private static final String VIEW_AUTHOR_FORM = "authors/viewAuthor";
 
     private final AuthorDbService dbService;
-
-    @PostMapping("/author/new")
-    public String addAuthor(@RequestBody AuthorDto authorDto, Model model) {
-        long id = dbService.save(new Author(authorDto.getName(), authorDto.getSurname()));
-        model.addAttribute("message", "Added author with id = " + id);
-        return ADD_AUTHOR_FORM;
-    }
 
     @GetMapping("/author/{id}")
     public String getAuthorById(@PathVariable("id") long id, Model model) {
