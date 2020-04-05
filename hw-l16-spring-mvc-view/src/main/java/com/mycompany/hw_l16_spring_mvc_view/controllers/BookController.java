d057d21d8029badcde7171a6c1cef178930b0c95
ysavchen <yura_savchenko@outlook.com>
2020-04-05 22:33:04 +0300

    Working addBook.html

--- hw-l16-spring-mvc-view/src/main/java/com/mycompany/hw_l16_spring_mvc_view/controllers/BookController.java
+++ hw-l16-spring-mvc-view/src/main/java/com/mycompany/hw_l16_spring_mvc_view/controllers/BookController.java
@@ -1,6 +1,8 @@
 package com.mycompany.hw_l16_spring_mvc_view.controllers;
 
+import com.mycompany.hw_l16_spring_mvc_view.dto.AuthorDto;
 import com.mycompany.hw_l16_spring_mvc_view.dto.BookDto;
+import com.mycompany.hw_l16_spring_mvc_view.dto.GenreDto;
 import com.mycompany.hw_l16_spring_mvc_view.service.BookDbService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -18,26 +20,41 @@ public class BookController {
 
     private static final String BOOK_LIST_FORM = "books/bookList";
     private static final String VIEW_BOOK_FORM = "books/viewBook";
-    private static final String ADD_BOOK_FORM = "books/new";
+    private static final String ADD_BOOK_FORM = "books/addBook";
 
     private final BookDbService dbService;
 
     @GetMapping("/book/new")
-    public String showAddForm() {
+    public String showAddForm(Model model) {
+        var book = new BookDto()
+                .setAuthor(new AuthorDto())
+                .setGenre(new GenreDto());
+        model.addAttribute("book", book);
         return ADD_BOOK_FORM;
     }
 
-    @PostMapping("/book/new")
-    public String addBook(@RequestBody BookDto bookDto, Model model) {
+    @PostMapping(value = "/book/new")
+    public String addBook(BookDto bookDto, Model model) {
         long id = dbService.save(BookDto.toDomainObject(bookDto));
         if (id != 0) {
             bookDto.setId(id);
-            model.addAttribute("message", "Book is successfully saved!");
+            model.addAttribute("message", "Book is saved with id = " + id + "!");
             model.addAttribute("book", bookDto);
+        } else {
+            model.addAttribute("message", "Book is not saved!");
+            model.addAttribute("book", null);
         }
-        model.addAttribute("message", "Book is not saved!");
-        model.addAttribute("book", null);
 
+        return VIEW_BOOK_FORM;
+    }
+
+    @PostMapping("/book/update/{id}")
+    public String updateTitle(@PathVariable("id") long id,
+                              @RequestBody BookDto bookDto,
+                              Model model) {
+        dbService.updateTitle(id, bookDto.getTitle());
+        model.addAttribute("message", "Title is updated!");
+        model.addAttribute("book", bookDto);
         return VIEW_BOOK_FORM;
     }
 
@@ -60,9 +77,8 @@ public class BookController {
         return VIEW_BOOK_FORM;
     }
 
-    @GetMapping("/book/delete/{id}")
-    public String deleteBook(@PathVariable("id") long id,
-                             Model model) {
+    @PostMapping("/book/delete/{id}")
+    public String deleteBook(@PathVariable("id") long id, Model model) {
         dbService.deleteById(id);
         var books = dbService.getAllBooks()
                 .stream()
