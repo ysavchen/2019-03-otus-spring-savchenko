acab446bb9f65d905dcf144e69c24dc00e481804
ysavchen <yura_savchenko@outlook.com>
2020-06-02 21:28:39 +0300

    Remove redundant repositories

--- hw-l20-spring-webflux/src/main/java/com/mycompany/hw_l20_spring_webflux/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.hw_l20_spring_webflux.repositories;
-
-import com.mycompany.hw_l20_spring_webflux.domain.Author;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface AuthorRepository extends ReactiveMongoRepository<Author, String> {
-}
--- hw-l20-spring-webflux/src/main/java/com/mycompany/hw_l20_spring_webflux/repositories/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.hw_l20_spring_webflux.repositories;
-
-import com.mycompany.hw_l20_spring_webflux.domain.Genre;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface GenreRepository extends ReactiveMongoRepository<Genre, String> {
-}
