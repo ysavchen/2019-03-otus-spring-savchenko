a260c44a3877f07f2dfaa36c918f97544b3f98d5
ysavchen <yura_savchenko@outlook.com>
2020-09-13 17:28:25 +0300

    Update login form

--- hw-l22-spring-security-auth/src/main/java/com/mycompany/hw_l22_spring_security_auth/security/SecurityConfiguration.java
+++ hw-l22-spring-security-auth/src/main/java/com/mycompany/hw_l22_spring_security_auth/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,12 +16,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-//    @Override
-//    public void configure(WebSecurity web) {
-//        web.ignoring().antMatchers("/");
-//    }
-
     private final UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/css/**");
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -30,9 +31,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .authenticated()
                 .and()
                 .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                .defaultSuccessUrl("/", true);
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/", true);
     }
 
     @Bean
