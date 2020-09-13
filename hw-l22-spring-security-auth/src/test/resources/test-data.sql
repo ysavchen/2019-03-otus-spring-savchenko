6130e1822fcd1615d0c218d44d5ba64e7bd65e2a
ysavchen <yura_savchenko@outlook.com>
2020-09-13 18:13:21 +0300

    Update test-data.sql

--- hw-l22-spring-security-auth/src/test/resources/test-data.sql
+++ hw-l22-spring-security-auth/src/test/resources/test-data.sql
@@ -6,3 +6,6 @@ insert into authors(id, name, surname) values (2, 'Michael', 'Learn');
 insert into books (id, title, author_id, genre_id) values (1, 'A Guide to SQL', 1, 1);
 insert into books (id, title, author_id, genre_id) values (2, 'Concepts of Database Management', 1, 1);
 insert into books (id, title, author_id, genre_id) values (3, 'SQL Programming and Coding', 2, 1);
+
+/* john.doe@test.com , #Start01 */
+insert into users (id, first_name, last_name, email, password) values (1, 'John', 'Doe', 'john.doe@test.com', '$2a$10$gfoYoDKAUG5qO8obPlvPlOe8F08HEszg.H0uVeam9ObC9eTeV46uy');
