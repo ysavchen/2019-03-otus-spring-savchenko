56ae9fb8b7318471a285f0fea09139297fc7e140
ysavchen <yura_savchenko@outlook.com>
2020-10-11 22:31:27 +0300

    Remove redundant classes

--- hw-l25-spring-batch/src/main/java/com/mycompany/hw_l25_spring_batch/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.hw_l25_spring_batch.repositories;
-
-import com.mycompany.hw_l25_spring_batch.domain.Author;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AuthorRepository extends MongoRepository<Author, String> {
-}
--- hw-l25-spring-batch/src/main/java/com/mycompany/hw_l25_spring_batch/repositories/BookRepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.hw_l25_spring_batch.repositories;
-
-public interface BookRepositoryCustom {
-
-    void updateTitle(String id, String title);
-}
