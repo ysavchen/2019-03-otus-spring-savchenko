5cc62ea2176882221ee1f05e8275eb059328b60e
ysavchen <yura_savchenko@outlook.com>
2020-10-22 19:00:39 +0300

    Add previous hw

---
+++ hw-l29-spring-boot-actuator/src/main/java/com/mycompany/hw_l29_spring_boot_actuator/dto/AuthorDto.java
@@ -0,0 +1,26 @@
+package com.mycompany.hw_l29_spring_boot_actuator.dto;
+
+import com.mycompany.hw_l29_spring_boot_actuator.domain.Author;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class AuthorDto {
+
+    private long id;
+    private String name;
+    private String surname;
+
+    public static Author toDomainObject(AuthorDto dto) {
+        return new Author(dto.id, dto.name, dto.surname);
+    }
+
+    public static AuthorDto toDto(Author author) {
+        return new AuthorDto(author.getId(), author.getName(), author.getSurname());
+    }
+}
